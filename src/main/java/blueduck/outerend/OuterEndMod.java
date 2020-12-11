b1f878ee64cf2f23c95e4fcf789a69b6f8e39414
Max <bludux595@gmail.com>
2020-12-11 14:24:13 -0500

    More setup

--- src/main/java/blueduck/outerend/OuterEndMod.java
+++ src/main/java/blueduck/outerend/OuterEndMod.java
@@ -19,13 +19,13 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
-public class ExampleMod
+@Mod("outer_end")
+public class OuterEndMod
 {
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
-    public ExampleMod() {
+    public OuterEndMod() {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
