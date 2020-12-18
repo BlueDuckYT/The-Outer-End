7d509caa6d2b3fd43c54c7d7e685f399a852b326
Max <bludux595@gmail.com>
2020-12-17 22:27:10 -0500

    Towers do not spawn and spam log

--- src/main/java/blueduck/outerend/OuterEndMod.java
+++ src/main/java/blueduck/outerend/OuterEndMod.java
@@ -2,22 +2,38 @@ package blueduck.outerend;
 
 import blueduck.outerend.client.ClientSetup;
 import blueduck.outerend.common.CommonSetup;
+import blueduck.outerend.features.ConfiguredStructureFeatures;
 import blueduck.outerend.registry.BiomeRegistry;
 import blueduck.outerend.registry.BlockRegistry;
 import blueduck.outerend.registry.EntityRegistry;
+import blueduck.outerend.registry.StructureRegistry;
 import blueduck.outerend.server.ServerStartup;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.gen.FlatChunkGenerator;
+import net.minecraft.world.gen.feature.structure.Structure;
+import net.minecraft.world.gen.settings.DimensionStructuresSettings;
+import net.minecraft.world.gen.settings.StructureSeparationSettings;
+import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.world.BiomeLoadingEvent;
+import net.minecraftforge.event.world.WorldEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLEnvironment;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Mod("outer_end")
 public class OuterEndMod
 {
-    private static final Logger LOGGER = LogManager.getLogger();
+    public static final Logger LOGGER = LogManager.getLogger();
     public static String MODID = "outer_end";
     public OuterEndMod() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
@@ -25,10 +41,15 @@ public class OuterEndMod
         FMLJavaModLoadingContext.get().getModEventBus().register(BiomeRegistry.class);
         MinecraftForge.EVENT_BUS.addListener(ServerStartup::onServerStarting);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(CommonSetup::onCommonSetup);
+        MinecraftForge.EVENT_BUS.addListener(EventPriority.NORMAL, this::addDimensionalSpacing);
+
+        MinecraftForge.EVENT_BUS.addListener(this::setup);
+        MinecraftForge.EVENT_BUS.addListener(EventPriority.HIGH, this::biomeModification);
     
         BiomeRegistry.init();
         BlockRegistry.init();
         EntityRegistry.init();
+        StructureRegistry.init();
 
         if (FMLEnvironment.dist.isClient()) {
             FMLJavaModLoadingContext.get().getModEventBus().addListener(ClientSetup::onSetup);
@@ -37,7 +58,58 @@ public class OuterEndMod
         MinecraftForge.EVENT_BUS.register(this);
     }
 
+    public void setup(final FMLCommonSetupEvent event)
+    {
+        event.enqueueWork(() -> {
+            StructureRegistry.setupStructures();
+            ConfiguredStructureFeatures.registerConfiguredStructures();
+        });
+    }
+
     private void doClientStuff(final FMLClientSetupEvent event) {
         LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().gameSettings);
+    }
+
+    public void biomeModification(final BiomeLoadingEvent event) {
+        // Add our structure to all biomes including other modded biomes.
+        // You can skip or add only to certain biomes based on stuff like biome category,
+        // temperature, scale, precipitation, mod id, etc. All kinds of options!
+        //
+        // You can even use the BiomeDictionary as well! To use BiomeDictionary, do
+        // RegistryKey.getOrCreateKey(Registry.BIOME_KEY, event.getName()) to get the biome's
+        // registrykey. Then that can be fed into the dictionary to get the biome's types.
+        if (event.getCategory().equals(Biome.Category.THEEND)) {
+            event.getGeneration().getStructures().add(() -> ConfiguredStructureFeatures.CONFIGURED_END_TOWER);
+        }
+    }
+
+    /**
+     * Will go into the world's chunkgenerator and manually add our structure spacing.
+     * If the spacing is not added, the structure doesn't spawn.
+     *
+     * Use this for dimension blacklists for your structure.
+     * (Don't forget to attempt to remove your structure too from
+     *  the map if you are blacklisting that dimension! It might have
+     *  your structure in it already.)
+     *
+     * Basically use this to make absolutely sure the chunkgenerator
+     * can or cannot spawn your structure.
+     */
+    public void addDimensionalSpacing(final WorldEvent.Load event) {
+        if (event.getWorld() instanceof ServerWorld) {
+            ServerWorld serverWorld = (ServerWorld) event.getWorld();
+
+            // Prevent spawning our structure in Vanilla's superflat world as
+            // people seem to want their superflat worlds free of modded structures.
+            // Also that vanilla superflat is really tricky and buggy to work with in my experience.
+            if (serverWorld.getChunkProvider().getChunkGenerator() instanceof FlatChunkGenerator &&
+                    serverWorld.getDimensionKey().equals(World.OVERWORLD)) {
+                return;
+            }
+
+            Map<Structure<?>, StructureSeparationSettings> tempMap = new HashMap<>(serverWorld.getChunkProvider().generator.func_235957_b_().func_236195_a_());
+            tempMap.put(StructureRegistry.END_TOWER.get(), DimensionStructuresSettings.field_236191_b_.get(StructureRegistry.END_TOWER.get()));
+            serverWorld.getChunkProvider().generator.func_235957_b_().field_236193_d_ = tempMap;
+        }
     }
 }
---
+++ src/main/java/blueduck/outerend/registry/StructureRegistry.java
@@ -0,0 +1,88 @@
+package blueduck.outerend.registry;
+
+import blueduck.outerend.OuterEndMod;
+import blueduck.outerend.structures.EndTowerStructure;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import net.minecraft.world.gen.feature.IFeatureConfig;
+import net.minecraft.world.gen.feature.NoFeatureConfig;
+import net.minecraft.world.gen.feature.structure.Structure;
+import net.minecraft.world.gen.settings.DimensionStructuresSettings;
+import net.minecraft.world.gen.settings.StructureSeparationSettings;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import java.util.function.Supplier;
+
+public class StructureRegistry {
+
+    public static final DeferredRegister<Structure<?>> STRUCTURES = DeferredRegister.create(ForgeRegistries.STRUCTURE_FEATURES, OuterEndMod.MODID);
+
+    public static final RegistryObject<Structure<NoFeatureConfig>> END_TOWER = setupStructure("end_tower", () -> (new EndTowerStructure(NoFeatureConfig.field_236558_a_)));
+
+    private static <T extends Structure<?>> RegistryObject<T> setupStructure(String name, Supplier<T> structure) {
+        return STRUCTURES.register(name, structure);
+    }
+
+    public static void init() {
+        STRUCTURES.register(FMLJavaModLoadingContext.get().getModEventBus());
+    }
+
+    public static void setupStructures() {
+        setupStructure(
+                END_TOWER.get(), /* The instance of the structure */
+                new StructureSeparationSettings(10 /* maximum distance apart in chunks between spawn attempts */,
+                        5 /* minimum distance apart in chunks between spawn attempts */,
+                        1234567990 /* this modifies the seed of the structure so no two structures always spawn over each-other. Make this large and unique. */),
+                true);
+
+        // Add more structures here and so on
+    }
+
+    public static <F extends Structure<?>> void setupStructure(
+            F structure,
+            StructureSeparationSettings structureSeparationSettings,
+            boolean transformSurroundingLand)
+    {
+        /*
+         * We need to add our structures into the map in Structure alongside vanilla
+         * structures or else it will cause errors. Called by registerStructure.
+         *
+         * If the registration is setup properly for the structure,
+         * getRegistryName() should never return null.
+         */
+        Structure.NAME_STRUCTURE_BIMAP.put(structure.getRegistryName().toString(), structure);
+
+        /*
+         * Whether surrounding land will be modified automatically to conform to the bottom of the structure.
+         * Basically, it adds land at the base of the structure like it does for Villages and Outposts.
+         * Doesn't work well on structure that have pieces stacked vertically or change in heights.
+         *
+         * Note: The air space this method will create will be filled with water if the structure is below sealevel.
+         * This means this is best for structure above sealevel so keep that in mind.
+         */
+        if(transformSurroundingLand){
+            Structure.field_236384_t_ =
+                    ImmutableList.<Structure<?>>builder()
+                            .addAll(Structure.field_236384_t_)
+                            .add(structure)
+                            .build();
+        }
+
+        /*
+         * Adds the structure's spacing into several places so that the structure's spacing remains
+         * correct in any dimension or worldtype instead of not spawning.
+         *
+         * However, it seems it doesn't always work for code made dimensions as they read from
+         * this list beforehand. Use the WorldEvent.Load event in StructureTutorialMain to add
+         * the structure spacing from this list into that dimension.
+         */
+        DimensionStructuresSettings.field_236191_b_ =
+                ImmutableMap.<Structure<?>, StructureSeparationSettings>builder()
+                        .putAll(DimensionStructuresSettings.field_236191_b_)
+                        .put(structure, structureSeparationSettings)
+                        .build();
+    }
+}
