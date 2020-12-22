f3da1326e693a2185183e40c5fd0068130baa150
Max <bludux595@gmail.com>
2020-12-22 10:51:29 -0500

    Dragon health

--- src/main/java/blueduck/outerend/mixin/DragonAttributesMixin.java
+++ src/main/java/blueduck/outerend/mixin/DragonAttributesMixin.java
@@ -1,5 +1,9 @@
 package blueduck.outerend.mixin;
 
+import net.minecraft.entity.MobEntity;
+import net.minecraft.entity.ai.attributes.AttributeModifierMap;
+import net.minecraft.entity.ai.attributes.Attributes;
+import net.minecraft.entity.boss.dragon.EnderDragonEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.registry.DynamicRegistries;
 import net.minecraft.world.DimensionType;
@@ -10,14 +14,16 @@ import net.minecraft.world.gen.feature.NoFeatureConfig;
 import net.minecraft.world.gen.feature.structure.EndCityStructure;
 import net.minecraft.world.gen.feature.template.TemplateManager;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(EndCityStructure.Start.class)
-public abstract class EndCityStartMixin {
-	@Inject(at = @At("HEAD"),method = "func_230364_a_(Lnet/minecraft/util/registry/DynamicRegistries;Lnet/minecraft/world/gen/ChunkGenerator;Lnet/minecraft/world/gen/feature/template/TemplateManager;IILnet/minecraft/world/biome/Biome;Lnet/minecraft/world/gen/feature/NoFeatureConfig;)V")
-	public void spawnGolems(DynamicRegistries registries, ChunkGenerator chunkGenerator, TemplateManager templateManager, int posX, int posZ, Biome biome, NoFeatureConfig config, CallbackInfo ci) {
-
+@Mixin(EnderDragonEntity.class)
+public abstract class DragonAttributesMixin {
+	@Overwrite
+	public static AttributeModifierMap.MutableAttribute registerAttributes() {
+		return MobEntity.func_233666_p_().createMutableAttribute(Attributes.MAX_HEALTH, 500.0D);
 	}
+
 }
