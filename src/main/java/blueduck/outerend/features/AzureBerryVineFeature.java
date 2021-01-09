7d5f6edb44e6157d3485057847ca747c4f816ad7
Max <bludux595@gmail.com>
2021-01-08 21:14:08 -0500

    Fixed berry vines

--- src/main/java/blueduck/outerend/features/AzureBerryVineFeature.java
+++ src/main/java/blueduck/outerend/features/AzureBerryVineFeature.java
@@ -23,24 +23,12 @@ public class AzureBerryVineFeature extends Feature<NoFeatureConfig> {
 
 	@Override
 	public boolean generate(ISeedReader reader, ChunkGenerator generator, Random rand, BlockPos pos, NoFeatureConfig config) {
-		int maxIterations = rand.nextInt(5) + 3;
-		for (int i = 0; i < maxIterations; i++) {
-			BlockPos buffer = pos;
-			pos = reader.getHeight(Heightmap.Type.WORLD_SURFACE_WG, pos.add(rand.nextInt(16), 0, rand.nextInt(16)));
-			if (reader.getBlockState(pos.down()).isIn(BlockTags.getCollection().getTagByID(new ResourceLocation("outer_end:end_plantable_on")))) {
-				int height = rand.nextInt(4);
-				if (reader.getBlockState(pos.up(height)).getBlock() == Blocks.AIR) {
-					for (int h = 0; h < height; h++) {
-						if (reader.getBlockState(pos.up(h)).getBlock() == Blocks.AIR || reader.getBlockState(pos.up(h).down()).getBlock() == BlockRegistry.AZURE_BERRY_VINE.get()) {
-							reader.setBlockState(pos.up(h), BlockRegistry.AZURE_BERRY_VINE.get().getDefaultState().with(AzureBerryVineBlock.AGE, rand.nextInt(4)), 4);
-						}
-					}
-					reader.setBlockState(pos.up(height), BlockRegistry.AZURE_BERRY_VINE_TOP.get().getDefaultState(), 4);
-				}
-			}
-			pos = buffer;
+		if (!reader.getBlockState(pos.down()).isIn(BlockTags.getCollection().getTagByID(new ResourceLocation("outer_end:end_plantable_on")))) return false;
+		int height = rand.nextInt(4);
+		for (int i = 0; i < height; i++) {
+			reader.setBlockState(pos.up(i), BlockRegistry.AZURE_BERRY_VINE.get().getDefaultState().with(AzureBerryVineBlock.AGE, rand.nextInt(4)), 4);
 		}
-
+		reader.setBlockState(pos.up(height), BlockRegistry.AZURE_BERRY_VINE_TOP.get().getDefaultState(), 4);
 		return true;
 	}
 }
