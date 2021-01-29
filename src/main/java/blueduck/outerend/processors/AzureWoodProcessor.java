e30548b6c40f2c0b507939150a14d29ccc8b808b
Max <bludux595@gmail.com>
2021-01-29 10:38:58 -0500

    Quark rune recipes for outer end crystals

---
+++ src/main/java/blueduck/outerend/processors/AzureWoodProcessor.java
@@ -0,0 +1,11 @@
+package blueduck.outerend.processors;
+
+import net.minecraft.world.gen.feature.template.IStructureProcessorType;
+import net.minecraft.world.gen.feature.template.StructureProcessor;
+
+public class AzureWoodProcessor extends StructureProcessor {
+    @Override
+    protected IStructureProcessorType<?> getType() {
+        return null;
+    }
+}
