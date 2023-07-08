11516e7fb00f597ba5504bc639097ecf2729449f
KingOfTheGamesYT <53309562+KingOfTheGamesYT@users.noreply.github.com>
2023-07-07 21:56:53 -0400

    Ported over the BlockEventListener class
    to 1.18.2

--- src/main/java/blueduck/outerend/server/BlockEventListener.java
+++ src/main/java/blueduck/outerend/server/BlockEventListener.java
@@ -1,20 +1,20 @@
 package blueduck.outerend.server;
 
 import blueduck.outerend.registry.BlockRegistry;
-import net.minecraft.block.Blocks;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
-import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.event.entity.player.BonemealEvent;
 import net.minecraftforge.event.world.BlockEvent;
 
 public class BlockEventListener {
 
     public static void onFluidChangeBlock(BlockEvent.FluidPlaceBlockEvent event) {
-        if (event.getWorld().getBlockState(event.getPos().down()).equals(BlockRegistry.ANCIENT_ICE.get().getDefaultState()) && event.getNewState().equals(Blocks.COBBLESTONE.getDefaultState())) {
-            event.setNewState(BlockRegistry.ANCIENT_STONE.get().getDefaultState());
+        if (event.getWorld().getBlockState(event.getPos().below()).equals(BlockRegistry.ANCIENT_ICE.get().defaultBlockState()) && event.getNewState().equals(Blocks.COBBLESTONE.defaultBlockState())) {
+            event.setNewState(BlockRegistry.ANCIENT_STONE.get().defaultBlockState());
         }
-        if (event.getWorld().getBlockState(event.getPos().down()).isIn(BlockTags.getCollection().getTagByID(new ResourceLocation("outer_end:crystal_blocks"))) && event.getNewState().equals(Blocks.COBBLESTONE.getDefaultState())) {
-            event.setNewState(BlockRegistry.VIOLITE.get().getDefaultState());
+        if (event.getWorld().getBlockState(event.getPos().below()).equals(BlockTags.create(new ResourceLocation("outer_end:crystal_blocks"))) && event.getNewState().equals(Blocks.COBBLESTONE.defaultBlockState())) {
+            event.setNewState(BlockRegistry.VIOLITE.get().defaultBlockState());
         }
     }
 
